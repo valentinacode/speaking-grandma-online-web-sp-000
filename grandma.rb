22bd478dd0a1a9274f72403ea1412fb6b09d5c7f
Valentina E <tiyahelfrih@gmail.com>
2020-07-20 11:59:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 def speak_to_grandma(string)
   if string != string.upcase
+    return "I LOVE YOU GRANDMA!"
+  elsif string.upcase
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+end 
+
+def speak_to_grandma(string)
+  if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
